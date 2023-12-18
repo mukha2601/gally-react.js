9fe154618a9fedaedde6dafd000c93f1c3314ce3
mukha <muhammaddin.xoliqov1998@gmail.com>
2023-12-18 16:56:07 +0500

    design, grid

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,24 @@
 
 export default function Home() {
    return (
-      <div className="w-full h-screen bg-blue-300">Home</div>
+      <div className="w-full h-auto bg-blue-300 grid grid-cols-[repeat(auto-fit,minmax(15rem,1fr))] auto-rows-[15rem]">
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+      </div>
    )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Main() {
    return (
-      <div className="2xl:container m-auto">
+      <div className="2xl:container m-auto px-3">
          <Navbar />
          <Outlet />
       </div>
